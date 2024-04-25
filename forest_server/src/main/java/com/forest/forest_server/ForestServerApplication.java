f730751bf8afb496e32a35fa0f164dcc8aac6475
keroro7729 <112931736+keroro7729@users.noreply.github.com>
2024-04-25 21:45:56 +0900

    Add files via upload

---
+++ forest_server/src/main/java/com/forest/forest_server/ForestServerApplication.java
@@ -0,0 +1,13 @@
+package com.forest.forest_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForestServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForestServerApplication.class, args);
+	}
+
+}
---
+++ forest_server/src/test/java/com/forest/forest_server/ForestServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forest.forest_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForestServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
