dde87da7cfda5f0402292cfd10dafa126827114c
JiHoon <kanda0101@naver.com>
2024-08-12 23:47:51 +0900

    프래그먼트1차

---
+++ Forest_app/app/src/main/java/com/example/forest_app/Fragment3.java
@@ -0,0 +1,28 @@
+package com.example.forest_app;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Fragment3 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_3, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
