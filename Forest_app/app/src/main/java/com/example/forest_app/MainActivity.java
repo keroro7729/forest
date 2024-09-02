eb9d1ae59c2b7938b76cbb138c17e7a0b3c09742
JiHoon <kanda0101@naver.com>
2024-09-02 23:51:46 +0900

    프래그먼트 추가

--- Forest_app/app/src/main/java/com/example/forest_app/MainActivity.java
+++ Forest_app/app/src/main/java/com/example/forest_app/MainActivity.java
@@ -18,7 +18,6 @@ import com.example.forest_app.form.RegisterForm;
 import com.example.forest_app.form.ResponseForm;
 import com.example.forest_app.utils.ImageLoader;
 
-import java.time.LocalDate;
 import java.util.Locale;
 
 import androidx.navigation.NavController;
@@ -37,123 +36,27 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
 
     private ApiManager apiManager = new ApiManager();
     private TextToSpeech tts;
+    private ImageView imageView;
+    private View fragmentContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        Button helloTestButton = findViewById(R.id.hello_test_button);
-
-        helloTestButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Call<ResponseForm> call = apiManager.getApiService().hello();
-                call.enqueue(new Callback<ResponseForm>() {
-                    @Override
-                    public void onResponse(Call<ResponseForm> call, Response<ResponseForm> response) {
-                        if(response.isSuccessful()){
-                            Log.d("Hello", "Request Info: "+call.request().toString());
-                            Log.d("Hello", "Received Message: "+response.body().getMessage());
-                        }
-                        else{
-                            Log.e("Hello", "Request Info: "+call.request());
-                            Log.e("Hello", "Fail code: "+response.code());
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(Call<ResponseForm> call, Throwable throwable) {
-                        Log.e("Hello", "Request Info: "+call.request());
-                        Log.e("Hello", "Network Error: "+throwable.getMessage());
-                    }
-                });
-            }
-        });
-
-        // image load testing
-        ImageView imageView = findViewById(R.id.imageView);
-        Bitmap bm;
-        Call<ResponseForm> call = apiManager.getApiService().getImage();
-        call.enqueue(new Callback<ResponseForm>() {
-            @Override
-            public void onResponse(Call<ResponseForm> call, Response<ResponseForm> response) {
-                if(response.isSuccessful()) {
-                    String data = response.body().getData();
-                    Log.d("image test button", "data: "+data);
-                    if(data != null) {
-                        Bitmap bm = ImageLoader.convertToBitMap(response.body().getData());
-                        imageView.setImageBitmap(bm);
-                    }
-                    else{
-                        Log.e("image test button", "data is null!!");
-                    }
-                }
-                else{
-                    Log.e("GetImage", "Request Info: "+call.request().toString());
-                    Log.e("GetImage", "Received Message: "+response.body().getMessage());
-                }
-            }
-
-            @Override
-            public void onFailure(Call<ResponseForm> call, Throwable t) {
-                Log.e("GetImage", "Request Info: "+call.request().toString());
-                Log.e("GetImage", "Network error: "+t.getMessage());
-            }
-        });
-
-        // tts testing
-        Button ttsButton = findViewById(R.id.tts_test_button);
-        tts = new TextToSpeech(this, this);
-        ttsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                RegisterForm form = new RegisterForm("1999-07-29", "M", "type1");
-
-                Call<AuthForm> call = apiManager.getApiService().createUser(form);
-                call.enqueue(new Callback<AuthForm>() {
-                    @Override
-                    public void onResponse(Call<AuthForm> call, Response<AuthForm> response) {
-                        if(response.isSuccessful()){
-                            AuthForm key = response.body();
-                            Log.d("createUser", "received key: "+key.toString());
-                        }
-                        else{
-                            Log.e("createUser", "Request Info: "+call.request().toString());
-                            Log.e("createUser", "Received Message: ");
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(Call<AuthForm> call, Throwable t) {
-                        Log.e("createUser", "Request Info: "+call.request().toString());
-                        Log.e("createUser", "Network Error: "+t.getMessage());
-                    }
-                });
-            }
-        });
-
+        fragmentContainer = findViewById(R.id.nav_host_fragment);
 
         BottomNavigationView navView = findViewById(R.id.bottom_navigation);
-        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.nav_host_fragment);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
         NavController navController = navHostFragment.getNavController();
-
-        View fragmentContainer = findViewById(R.id.nav_host_fragment);
 
         // BottomNavigationView와 NavController 연결
         NavigationUI.setupWithNavController(navView, navController);
 
-        navView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                // ImageView를 숨기고 FragmentContainerView를 보여줌
-                imageView.setVisibility(View.GONE);
-                fragmentContainer.setVisibility(View.VISIBLE);
-                return NavigationUI.onNavDestinationSelected(item, navController);
-            }
+        navView.setOnNavigationItemSelectedListener(item -> {
+            fragmentContainer.setVisibility(View.VISIBLE);
+            return NavigationUI.onNavDestinationSelected(item, navController);
         });
     }
 
@@ -166,18 +69,17 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
     @Override
     public void onInit(int status) {
         if (status == TextToSpeech.SUCCESS) {
-            int language = tts.setLanguage(Locale.KOREAN);  // 언어 설정
+            int language = tts.setLanguage(Locale.KOREAN);
             if (language == TextToSpeech.LANG_MISSING_DATA
                     || language == TextToSpeech.LANG_NOT_SUPPORTED) {
-                Log.e("MainActivity.onInit()", "unsported language");
+                Log.e("MainActivity.onInit()", "Unsupported language");
             }
         } else {
-            Log.e("MainActivity.onInit()", "initialization failed");
+            Log.e("MainActivity.onInit()", "Initialization failed");
         }
     }
 
     @Override
-
     protected void onDestroy() {
         if (tts != null) {
             tts.stop();
